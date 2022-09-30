e121bd01515dc5c597e88df2681adb33013218fc
Sayan Mondal <sayan.mondal@harness.io>
2022-09-30 21:55:57 +0530

    feat: Adding Sidenav and basic chat

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Camera from '../assets/video-call.png';
+import AddFriend from '../assets/add-friend.png';
+import More from '../assets/more.png';
 
-const Chat = () => {
-  return <div className="chat">Chat</div>;
+const Chat: React.FC = () => {
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Jane</span>
+        <div className="chatIcons">
+          <img src={Camera} alt="Camera" />
+          <img src={AddFriend} alt="Add" />
+          <img src={More} alt="settings" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
