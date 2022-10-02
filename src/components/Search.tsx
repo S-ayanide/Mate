8954a5d5a1ccd7ef3984570cfeaaa5272f21185a
Sayan Mondal <sayan.mondal@harness.io>
2022-10-02 18:51:30 +0530

    chore: Adding clear button to input

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,35 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 // eslint-disable-next-line import/named
-import { collection, DocumentData, getDocs, query } from 'firebase/firestore';
-import React, { useState } from 'react';
+import { User } from 'firebase/auth';
+import {
+  collection,
+  doc,
+  // eslint-disable-next-line import/named
+  DocumentData,
+  getDoc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+} from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
 import { db } from '../config';
+import Cancel from '../assets/cancel.png';
 
 const Search: React.FC = () => {
   const [username, setUsername] = useState<string>('');
-  const [users, setUsers] = useState<DocumentData[]>();
-  const [err, setError] = useState<boolean>(false);
+  const [users, setUsers] = useState<DocumentData[] | undefined>(undefined);
+
+  const [currentUser, setCurrentUser] = useState<User | null>();
+
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+    if (user) {
+      setCurrentUser(JSON.parse(user));
+    }
+  }, []);
 
   const handleSearch = async () => {
     const q = query(collection(db, 'users'));
@@ -21,13 +44,51 @@ const Search: React.FC = () => {
       });
       setUsers(searchResult);
     } catch (err) {
-      setError(true);
+      console.error(err);
     }
   };
 
   const handleKey = (event: React.KeyboardEvent<HTMLInputElement>) => {
     event.code === 'Enter' && handleSearch();
   };
+
+  const handleSelect = async (user: DocumentData) => {
+    let combinedUID;
+    if (currentUser) {
+      try {
+        combinedUID = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid;
+        const res = await getDoc(doc(db, 'chats', combinedUID));
+        if (!res.exists()) {
+          // Create a new chat
+          await setDoc(doc(db, 'chats', combinedUID), { messages: [] });
+          // Create User chats
+          await updateDoc(doc(db, 'userChats', currentUser.uid), {
+            [combinedUID + '.userInfo']: {
+              uid: user.uid,
+              displayName: user.displayName,
+              photoURL: user.photoURL,
+            },
+            [combinedUID + '.date']: serverTimestamp(),
+          });
+          await updateDoc(doc(db, 'userChats', user.uid), {
+            [combinedUID + '.userInfo']: {
+              uid: currentUser.uid,
+              displayName: currentUser.displayName,
+              photoURL: currentUser.photoURL,
+            },
+            [combinedUID + '.date']: serverTimestamp(),
+          });
+        }
+      } catch (err) {
+        console.error(err);
+      }
+
+      setUsername('');
+      setUsers(undefined);
+    }
+  };
+
+  console.log(users);
 
   return (
     <div className="search">
@@ -37,11 +98,22 @@ const Search: React.FC = () => {
           placeholder="Find a user"
           onKeyDown={handleKey}
           onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+        <img
+          src={Cancel}
+          alt="cross"
+          onClick={() => {
+            setUsername('');
+            setUsers(undefined);
+          }}
         />
       </div>
+      {users && users.length === 0 && <p>No user found</p>}
       {users &&
         users.map((user) => (
-          <div key={user.uid} className="userChat">
+          // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/interactive-supports-focus
+          <div key={user.uid} className="userChat" onClick={() => handleSelect(user)} role="button">
             <img src={user?.photoURL} alt={user?.photoURL} />
             <div className="userChatInfo">
               <span>{user?.displayName}</span>
