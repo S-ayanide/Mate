9516c94951c528be365894425b9a83abbd35b957
Sayan Mondal <sayan.mondal@harness.io>
2022-10-09 20:41:46 +0530

    feat: Adding smtpJS

--- src/components/AddFriend.tsx
+++ src/components/AddFriend.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import emailjs from '@emailjs/browser';
 import Checked from '../assets/checked.png';
+import ReactDOMServer from 'react-dom/server';
 import { useStoreState } from '../store';
 
 const AddFriend: React.FC = () => {
@@ -14,31 +14,58 @@ const AddFriend: React.FC = () => {
     const target = e.target as HTMLInputElement;
     if (Object.values(target)[0].value === '') setError('Please enter an email address');
     else {
-      // eslint-disable-next-line import/no-named-as-default-member
-      emailjs
-        .send(
-          import.meta.env.VITE_EMAILJS_SERVICE_ID,
-          import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-          {
-            from_name: currentUser?.displayName,
-            to_name: Object.values(target)[0].value,
-            to_email: Object.values(target)[0].value,
-            reply_to: Object.values(target)[0].value,
-          },
-          import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
-        )
-        .then(
-          (result) => {
-            if (result.status === 200) {
-              setError('');
-              setIsEmailSent(true);
-            }
-          },
-          (error) => {
-            setError(error.text);
-            setIsEmailSent(false);
-          },
-        );
+      const emailBody = ReactDOMServer.renderToStaticMarkup(
+        <div style={{ backgroundColor: 'rgb(237,218,205)', width: '100%' }}>
+          <div className="centerText">
+            <img
+              style={{ width: '150px', height: 'auto', marginLeft: '2.5%' }}
+              src="https://i.ibb.co/SvXD6Dy/mote.png"
+              alt="Mate"
+            />
+          </div>
+          <div style={{ backgroundColor: 'white', width: '90%', padding: '10px', marginLeft: '3%' }}>
+            <p>
+              Hello {Object.values(target)[0].value},
+              <br />
+              <br />
+              Mate is a new age, instant messaging service that also provides optional end-to-end encrypted chats and
+              video calling, file sharing, and several other features.
+            </p>
+            <p>
+              has given you a special invite to join them on Mate, and carry on the conversation.
+              <br />
+              <br />
+              Don&apos;t miss out on this opportunity and click here to accept your invitation:{' '}
+              <a href="http://localhost:5173/signup" target="_blank" rel="noopener noreferrer">
+                Accept Invitation
+              </a>
+              <br />
+              <br />
+            </p>
+          </div>
+          <br />
+          <p style={{ fontSize: '12px', marginLeft: '3%', paddingBottom: '3%' }}>
+            Best wishes,
+            <br />
+            Sayan
+            <br />
+            (Creator of Mate)
+          </p>
+        </div>,
+      );
+
+      Email.send({
+        Host: 'smtp.elasticemail.com',
+        Username: import.meta.env.VITE_SMTPJS_SMTP_USERNAME,
+        Password: import.meta.env.VITE_SMTPJS_SMTP_PASSWORD,
+        To: Object.values(target)[0].value,
+        From: import.meta.env.VITE_SMTPJS_SMTP_USERNAME,
+        Subject: `${currentUser?.displayName} has invited you to join Mate`,
+        Body: emailBody,
+      }).then(() => {
+        setError('');
+        setIsEmailSent(true);
+      });
     }
   };
 
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,31 @@
 /// <reference types="vite/client" />
+
+// Type definitions for SmtpJs
+// Project: https://smtpjs.com/
+// Definitions by: Linda Paiste https://github.com/lindapaiste
+
+// SmtpJS exposes a variable `Email` on the global `window` object
+declare namespace Email {
+  type Attachment =
+    | {
+        name: string;
+        path: string;
+      }
+    | {
+        name: string;
+        data: string; // base64 format
+      };
+
+  interface EmailData {
+    Username: string;
+    Password: string;
+    Host: string;
+    To: string | string[];
+    From: string;
+    Subject: string;
+    Body: string | HTML;
+    Attachments?: Attachment[];
+  }
+
+  function send(email: EmailData): Promise<string>;
+}
